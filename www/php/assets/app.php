0d22a84bd5b9f826730ebf180e2f88082bd3be79
Matti van de Weem <mattiweem@hotmail.com>
2015-04-07 22:00:58 +0200

    5.3.3...

--- www/php/assets/app.php
+++ www/php/assets/app.php
@@ -30,7 +30,7 @@ class app{
 	}
 
 	public function response(){
-		return json_encode(['succes' => $this->status, 'data' => $this->data]);
+		return json_encode(array('succes' => $this->status, 'data' => $this->data));
 	}
 
 
