9458135a9d5041e62905a1d3f88d196299e41d33
Koen Vendrik <k.vendrik@gmail.com>
2015-04-07 13:39:44 +0200

    Adds email form, still need to actual POST data to server

--- dev/js/src/main.js
+++ dev/js/src/main.js
@@ -2,4 +2,60 @@
 
     'use strict';
 
+    var _gebi = function(id){
+        return document.getElementById(id);
+    };
+
+    //email
+    (function(){
+
+        var emailInput = _gebi('email-input'),
+            sendBtn = _gebi('email-send-btn'),
+
+            BsendBtn = B(sendBtn),
+
+            performingState = false,
+            showState = function(stateName, freezeOnState){
+                performingState = true;
+
+                BsendBtn.addClass('is-hidden');
+                setTimeout(function(){
+                    BsendBtn.removeClass('is-hidden');
+                    BsendBtn.addClass('is-'+stateName);
+
+                    if(!freezeOnState){
+                        setTimeout(function(){
+                            BsendBtn.addClass('is-hidden');
+                            setTimeout(function(){
+                                BsendBtn.removeClass('is-'+stateName);
+                                BsendBtn.removeClass('is-hidden');
+                                performingState = false;
+                            }, 300);
+                        }, 1000);
+                    }
+                }, 300);
+            };
+
+        BsendBtn.click(function(e){
+            e.preventDefault();
+            if(performingState) return;
+
+            var val = emailInput.value;
+
+            if(!/.+\@.+\..+/.test(val)){
+                showState('error');
+                return;
+            }
+
+            //send email
+            BsendBtn.addClass('is-mailing');
+
+            setTimeout(function(){
+                BsendBtn.removeClass('is-mailing');
+                showState('success', true);
+            }, 5000);
+        });
+
+    }());
+
 }());
